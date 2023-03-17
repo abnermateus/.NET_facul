fb222829a38f0db9b00165ec16e1ebc7c882c9b2
abnermateus <75183543+abnermateus@users.noreply.github.com>
2023-03-17 18:58:26 -0300

    Initial commit

---
+++ Lista0/exercicios.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace C_.Lista0
+{
+    class exercicios
+    {
+        static void Main(String[] args)
+        {
+            /*EX1
+            Console.Write("Informe o valor em metros: ");
+            double metros = double.Parse(Console.ReadLine());
+            Console.Write(metros + " metros = " + metros * 100 + " centímetros");
+            */
+
+            /*EX2
+            Console.Write("Informe a temperatura em Fahrenheit: ");
+            double f = double.Parse(Console.ReadLine());
+            Console.Write(f + " °F = " + (f - 32) * 5 / 9 + " °C");
+            */
+
+            /*EX3
+            Console.Write("Informe a 1ª nota: ");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Informe o peso da 1ª nota: ");
+            double peso1 = double.Parse(Console.ReadLine());
+            Console.Write("\n\nInforme a 2ª nota: ");
+            double nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Informe o peso da 2ª nota: ");
+            double peso2 = double.Parse(Console.ReadLine());
+            Console.Write("\n\nInforme a 3ª nota: ");
+            double nota3 = double.Parse(Console.ReadLine());
+            Console.Write("Informe o peso da 3ª nota: ");
+            double peso3 = double.Parse(Console.ReadLine());
+
+            double m_pond = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / peso1 + peso2 + peso3;
+            Console.Write("\nA média ponderada das notas é " + m_pond);
+            */
+
+            /*EX4
+            Console.Write("Informe a quantidade de eleitores: ");
+            int eleit = int.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de votos brancos: ");
+            double branc = int.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de votos nulos: ");
+            double nul = int.Parse(Console.ReadLine());
+            Console.Write("Informe a quantidade de votos válidos: ");
+            double val = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + branc / eleit * 100 + "% de votos brancos");
+            Console.WriteLine(nul / eleit * 100 + "% de votos nulos");
+            Console.WriteLine(val / eleit * 100 + "% de votos válidos");
+            */
+
+            /*EX5
+            Console.Write("Informe o salário do funcionário: ");
+            double sal = double.Parse(Console.ReadLine());
+            double nv_sal = sal * 0.25;
+            Console.Write("O novo salário do funcionário com 25% de reajuste é: " + (sal + nv_sal));
+            */
+
+            /*EX6
+            Console.Write("Informe o valor das despesas: ");
+            double despesas = double.Parse(Console.ReadLine());
+            double total = despesas + (despesas * 0.10);
+
+            Console.Write("\nO valor gasto com gorjeta é R$ " + despesas * 0.10 + ".\nO valor total a ser pago é R$ " + total + ".");
+            */
+        }
+    }
+}
