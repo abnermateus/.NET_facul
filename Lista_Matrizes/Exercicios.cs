384d8a5be9839e219b92c728c81b8b093b51ffdf
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2023-04-18 09:28:14 -0300

    commit

--- Lista_Matrizes/Exercicios.cs
+++ Lista_Matrizes/Exercicios.cs
@@ -4,6 +4,18 @@ namespace Lista_Matrizes
 {
     class Exercicios
     {
-        static void main
+       static void Main(string[] args)
+        {
+            int[,] m = new int[4, 5];
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    Console.Write("Digite o elemento da linha {0} e coluna {1}: ", i, j);
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
     }
 }
