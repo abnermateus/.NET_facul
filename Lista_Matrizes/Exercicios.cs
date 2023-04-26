3332026377250c465db655ca6809841eb0014b48
abnermateus <75183543+abnermateus@users.noreply.github.com>
2023-04-25 22:49:28 -0300

    commit

--- Lista_Matrizes/Exercicios.cs
+++ Lista_Matrizes/Exercicios.cs
@@ -4,6 +4,7 @@ namespace C_.Lista_Matrizes
 {
     public class Exercicios
     {
+        /*
         static void Main(String[] args)
         {
             /*Ex1
@@ -31,10 +32,9 @@ namespace C_.Lista_Matrizes
             Console.WriteLine("A soma de todos os elementos é {0}", soma_total);
             */
 
-            //Dada uma matriz A[4x4], imprimir o número de linhas e o número de colunas nulas (com apenas 
-            //0s) da matriz. Uma estrutura de repetição deve ser utilizada para percorrer as linhas e colunas.
+            /*Ex2
             int[,] A = new int[4, 4];
-            int soma_total = 0, cont_linha = 0, cont_coluna = 0;
+            int soma = 0, cont_l = 0, cont_c = 0;
 
             for (int i = 0; i < 4; i++)
             {
@@ -42,34 +42,89 @@ namespace C_.Lista_Matrizes
                 {
                     Console.Write("Digite o elemento da linha {0} e coluna {1}: ", i, j);
                     A[i, j] = int.Parse(Console.ReadLine());
+                    soma += A[i, j];
                 }
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
+                if (soma == 0)
                 {
-                    soma_total += A[i, j];
+                    cont_l++;
                 }
-                if (soma_total == 0)
-                {
-                    cont_linha++;
-                }
-                soma_total = 0;
+                soma = 0;
             }
+
             for (int j = 0; j < 4; j++)
             {
                 for (int i = 0; i < 4; i++)
                 {
-                    soma_total += A[i, j];
+                    soma += A[i, j];
                 }
-                if (soma_total == 0)
+                if (soma == 0)
                 {
-                    cont_coluna++;
+                    cont_c++;
                 }
-                soma_total = 0;
+                soma = 0;
             }
-            Console.WriteLine("O número de linhas nulas é {0} e o número de colunas nulas é {1}", cont_linha, cont_coluna);
+            Console.WriteLine("Esta matriz tem {0} linha(s) nula(s)", cont_l);
+            Console.WriteLine("Esta matriz tem {0} coluna(s) nula(s)", cont_c);
+            */
+
+            /*Ex3
+            Console.Write("Digite a dimensão da matriz quadrada: ");
+            int t = int.Parse(Console.ReadLine());
+            int[,] m = new int[t, t];
+
+            for(int i = 0; i < t; i++)
+            {
+                for(int j = 0; j < t; j++)
+                {
+                    Console.Write("Digite o elemento da linha {0} e coluna {1}: ", i, j);
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.Write("Os elementos da diagonal são: ");
+            for(int i = 0; i < t; i++)
+            {
+                Console.Write("{0} ", m[i, i]);
+            }
+            Console.WriteLine();
+            */
+
+            /*Ex4
+            Console.WriteLine("Digite a dimensão das matrizes quadradas: ");
+            int t = int.Parse(Console.ReadLine());
+            int[,] A = new int[t, t], B = new int[t, t], C = new int[t, t];
+
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write("Digite o elemento da linha {0} e coluna {1} da matriz A: ", i, j);
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write("Digite o elemento da linha {0} e coluna {1} da matriz B: ", i, j);
+                    B[i, j] = int.Parse(Console.ReadLine());
+                    C[i, j] = A[i, j] * B[i, j];
+                }
+            }
+
+            Console.Write("\nA matriz C = A * B é igual a: ");
+            for (int i = 0; i < t; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write("{0} ", C[i, j]);
+                }
+            }
+            Console.WriteLine();
         }
+         */
     }
 }
 
