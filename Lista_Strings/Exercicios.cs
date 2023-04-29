dda9beae25d9e42e787630ee30bf788ada5abfed
abnermateus <75183543+abnermateus@users.noreply.github.com>
2023-04-29 16:54:40 -0300

    commit

---
+++ Lista_Strings/Exercicios.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace C_.Lista_Strings
+{
+    public class Exercicios
+    {
+        static void Main(String[] args)
+        {
+            /*Ex1
+            string str1, str2, maior = "";
+            Console.Write("Digite a primeira string: ");
+            str1 = Console.ReadLine();
+            Console.Write("Digite a segunda string: ");
+            str2 = Console.ReadLine();
+
+            if (str1.Equals(str2))
+            {
+                Console.WriteLine("\nStrings iguais");
+            }
+            else
+            {
+                if (str1.Length > str2.Length)
+                {
+                    maior = str1;
+                }
+                else
+                {
+                    maior = str2;
+                }
+                Console.WriteLine("Maior string: " + maior);
+                Console.WriteLine("Strings concatenadas: " + str1 + str2);
+                Console.WriteLine("Strings concatenadas: " + str2 + str1);
+            }
+            */
+
+            
+        }
+    }
+}
