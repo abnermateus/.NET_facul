ed9be932d6945bba1222b5119597cd5a6d74a568
abnermateus <75183543+abnermateus@users.noreply.github.com>
2023-05-16 23:36:54 -0300

    commit

--- Lista_Funcoes/Ex1.cs
+++ Lista_Funcoes/Ex1.cs
@@ -2,34 +2,34 @@ using System;
 
 namespace Lista_Funcoes
 {
-    class Exercicios
+    class Ex1
     {
-        void media(char tip_med, double n1, double n2, double n3) 
+        static void Media(char tip_med, double n1, double n2, double n3)
         {
             double result;
-            
-            if(tip_med == 'A' || tip_med == 'a') 
+
+            if (tip_med == 'A' || tip_med == 'a')
             {
                 result = (n1 + n2 + n3) / 3;
                 Console.WriteLine("A média aritmética de {0}, {1}, {2} é: {3}", n1, n2, n3, result);
             }
-            else if(tip_med == 'P' || tip_med == 'p') 
+            else if (tip_med == 'P' || tip_med == 'p')
             {
-                result = (n1 * 5 + n2 * 3 + n3 * 2) / 5 + 3 + 2;
+                result = (n1 * 5 + n2 * 3 + n3 * 2) / (5 + 3 + 2);
                 Console.WriteLine("A média ponderada de {0}, {1}, {2} é: {3}", n1, n2, n3, result);
             }
         }
-
-        public static void Main(String[] args) 
+        /*
+        public static void Main(string[] args)
         {
             Console.Write("Informe a quantidade de alunos: ");
             int quant = int.Parse(Console.ReadLine());
             char opcao;
 
-            for(int i = 0; i < quant; i++) 
+            for (int i = 0; i < quant; i++)
             {
-                Console.WriteLine("A ou a para média aritmética");
-                Console.WriteLine("P ou p para média aritmética");
+                Console.WriteLine("\nA ou a para média aritmética");
+                Console.WriteLine("P ou p para média ponderada");
                 Console.Write("Digite uma opção: ");
                 opcao = char.Parse(Console.ReadLine());
 
@@ -39,12 +39,10 @@ namespace Lista_Funcoes
                 double n2 = int.Parse(Console.ReadLine());
                 Console.Write("Informe a 3º nota do aluno: ");
                 double n3 = int.Parse(Console.ReadLine());
-                
-              
-                media(opcao, n1, n2, n3);
-                
-            }
 
-        }
+                Media(opcao, n1, n2, n3);
+                Console.WriteLine();
+            }
+        } */
     }
 }
---
+++ Lista_Funcoes/Ex2.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Lista_Funcoes
+{
+    class Ex2
+    {
+        static void Populacao()
+        {
+            int filhos, pessoas = 0;
+            double salario, sal_tot = 0, media = 0;
+
+            do
+            {
+                Console.Write("Informe o salário: ");
+                salario = double.Parse(Console.ReadLine());
+
+                if (salario < 0)
+                {
+                    break;
+                }
+                sal_tot += salario;
+                pessoas += 1;
+                Console.Write("Informe o número de filhos: ");
+                filhos = int.Parse(Console.ReadLine());
+                pessoas += filhos;
+
+                media += pessoas / sal_tot;
+                Console.WriteLine(media + "\n");
+            } while (salario >= 0);
+        }
+
+        public static void Main(string[] args)
+        {
+            Populacao();
+        }
+    }
+}
