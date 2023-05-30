77ec39a611b7311403d9f4230a6b928c9df55d8a
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2023-05-30 09:30:35 -0300

    commit

---
+++ Lista_Arquivos/Exercicios.cs
@@ -0,0 +1,14 @@
+using System;
+using System.io;
+using System.text;
+
+namespace C_.Lista_Arquivos
+{
+    public class Exercicios
+    {
+        static void Main(String[] args) 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
